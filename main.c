03e82bf18c46ff8a7fccccbc11a862f028fcbd16
Emile_Guillermin <emile.guillermin@gmail.com>
2023-12-01 09:11:29 +0100

    Push_V4

--- main.c
+++ main.c
@@ -58,22 +58,58 @@ int main() {
     }
 
     for (int i = 0; i < nombre_de_sommet; i++) {
-        for (int j = 0; j < nb_precedence_arbre; j++) {
+        for (int j = 0; j <= nb_precedence_arbre; j++) {
             fscanf(fichier_2, "%d%d", &sommet_1, &sommet_2);
+            printf("\n [%d]  [%d]\n",monArbre[i]->sommet, sommet_1);
             if(monArbre[i]->sommet == sommet_1){
                 monArbre[i]->nb_successeur++;
+
             }
             else if(monArbre[i]->sommet == sommet_2){
                 monArbre[i]->nb_predecesseur++;
             }
         }
+        rewind(fichier_2);
     }
-
+/*
     for (int i = 0; i < nombre_de_sommet; i++) {
         printf("\nnombre de successeur avec le sommet [%d] = [%d]\n", monArbre[i]->sommet, monArbre[i]->nb_successeur);
         printf("\nnombre de predecesseur avec le sommet [%d] = [%d]\n", monArbre[i]->sommet, monArbre[i]->nb_predecesseur);
     }
+*/
+    for (int i = 0; i < nombre_de_sommet; i++) {
+        monArbre[i]->successeur = (int*)malloc(sizeof(int)*monArbre[i]->nb_successeur);
+        monArbre[i]->predecesseur = (int*)malloc(sizeof(int)*monArbre[i]->nb_predecesseur);
+    }
 
+    rewind(fichier_2);
+    int indentation_successeur = 0;
+    int indentation_predecesseur = 0;
+
+    for (int i = 0; i < nombre_de_sommet; i++) {
+        for (int j = 0; j <= nb_precedence_arbre; j++) {
+            fscanf(fichier_2, "%d%d", &sommet_1, &sommet_2);
+            //printf("\n [%d]  [%d]\n",monArbre[i]->sommet, sommet_1);
+            if(monArbre[i]->sommet == sommet_1){
+                monArbre[i]->successeur[indentation_successeur] = sommet_2;
+                indentation_successeur++;
+            }
+            else if(monArbre[i]->sommet == sommet_2){
+                monArbre[i]->predecesseur[indentation_predecesseur] = sommet_1;
+                indentation_predecesseur++;
+            }
+        }
+        indentation_successeur = 0;
+        indentation_predecesseur = 0;
+        rewind(fichier_2);
+    }
+
+    for (int i = 0; i < nombre_de_sommet; i++) {
+        printf("\nMon sommet [%d] : ", monArbre[i]->sommet);
+        for (int j = 0; j < monArbre[i]->nb_successeur; j++) {
+            printf(" [%d] ", monArbre[i]->successeur[j]);
+        }
+    }
 
     return 0;
 }
