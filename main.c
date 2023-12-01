00ee3123962660b34002560dbe4d2e1048ecd0c2
Emile_Guillermin <emile.guillermin@gmail.com>
2023-12-01 08:34:11 +0100

    Push_V3

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ int main() {
     FILE *fichier;
     FILE *fichier_2;
     char caractere;
-    int numero_du_sommet =0;
-    int nombre_de_sommet = 0;
-    int nb_precedence_arbre = 1;
+    int numero_du_sommet = 0;
+    int nombre_de_sommet = 1; // il y a forcement 1 sommet à la base
+    int nb_precedence_arbre = 0; // mais il peut y avoir 0 predecésseur
     float temps;
     int sommet_1 = 0;
     int sommet_2 = 0;
@@ -20,6 +20,7 @@ int main() {
         }
     }
     rewind(fichier);
+    printf("\nordre du graphe : [%d]\n\n", nombre_de_sommet);
 
     arbre* monArbre[nombre_de_sommet];
 
@@ -35,70 +36,43 @@ int main() {
         fscanf(fichier, "%d%f", &numero_du_sommet, &temps);
         monArbre[j]->sommet = numero_du_sommet;
         monArbre[j]->temps_execution = temps;
-
     }
 
     rewind(fichier);
+    for (int i = 0; i < nombre_de_sommet; i++) {
+        printf("\n[%d] = [%f]\n", monArbre[i]->sommet, monArbre[i]->temps_execution);
+    }
+
+
 
     while ((caractere = fgetc(fichier_2)) != EOF) {
         if (caractere == '\n') {
             nb_precedence_arbre++;
         }
     }
-    nb_precedence_arbre++;
+    printf("\ntaille du graphe : [%d]\n\n", nb_precedence_arbre);
     rewind(fichier_2);
-
-    int tab_sommet_provisoir[nb_precedence_arbre][nb_precedence_arbre];
-    for (int i = 0; i < nb_precedence_arbre; i++) {
-        for (int j = 0; j < nb_precedence_arbre; j++) {
-            tab_sommet_provisoir[i][j] = 0;
-        }
+    for (int i = 0; i < nombre_de_sommet; i++ ) {
+        monArbre[i]->nb_predecesseur = 0;
+        monArbre[i]->nb_successeur = 0;
     }
-    for (int i = 0; i < nb_precedence_arbre; i++) {
+
+    for (int i = 0; i < nombre_de_sommet; i++) {
+        for (int j = 0; j < nb_precedence_arbre; j++) {
             fscanf(fichier_2, "%d%d", &sommet_1, &sommet_2);
-            tab_sommet_provisoir[sommet_1][sommet_2] = 12345;
-        printf("\n on complete la case : [%d][%d]\n", sommet_1, sommet_2);
-        }
-
-    for (int i = 0; i < nb_precedence_arbre; i++) {
-        printf("\n");
-        for (int j = 0; j < nb_precedence_arbre; j++) {
-            printf(" %d ", tab_sommet_provisoir[i][j]);
-            if (tab_sommet_provisoir[i][j] == 12345){
-               monArbre[i]->nb_successeur++;
+            if(monArbre[i]->sommet == sommet_1){
+                monArbre[i]->nb_successeur++;
+            }
+            else if(monArbre[i]->sommet == sommet_2){
+                monArbre[i]->nb_predecesseur++;
             }
         }
-        monArbre[i]->successeur = (int*)malloc(sizeof(int)*monArbre[i]->nb_successeur);
     }
 
-    //printf("\n\ncase a probleme %d", tab_sommet_provisoir[26][28]);
-
-
-
-    //reserve de l'espace mémoir pour les précédence et les succésseur
-
-/*
     for (int i = 0; i < nombre_de_sommet; i++) {
-        for (int j = 0; j < nb_precedence; j++) {
-            fscanf(fichier_2, "%d", &sommet);
-            if (sommet == monArbre[i]->sommet){
-                successeur++;
-            }
-            while ((caractere = fgetc(fichier_2)) != '\n' && caractere != EOF) {
-                // Ignorer les caractères restants sur la ligne
-            }
-        }
-        monArbre[i]->nb_successeur = successeur;
-        monArbre[i]->successeur = (int*)malloc(sizeof(int)*successeur);
-        successeur = 0;
+        printf("\nnombre de successeur avec le sommet [%d] = [%d]\n", monArbre[i]->sommet, monArbre[i]->nb_successeur);
+        printf("\nnombre de predecesseur avec le sommet [%d] = [%d]\n", monArbre[i]->sommet, monArbre[i]->nb_predecesseur);
     }
-
-*/
-    for (int i = 0; i < nombre_de_sommet; i++) {
-        printf("\n nombre de sucesseur du sommet [%d] : [%d]\n", monArbre[i]->sommet, monArbre[i]->nb_successeur);
-    }
-
-
 
 
     return 0;
