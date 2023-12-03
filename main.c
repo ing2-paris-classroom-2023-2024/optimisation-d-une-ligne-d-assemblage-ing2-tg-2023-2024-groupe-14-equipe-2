ca43152626a9a7e26a059d4c5b6078661acbce0a
David Stan <xblaster2407@gmail.com>
2023-12-03 20:23:16 +0100

    suppression des .c et .h inutiles, maj cmake /V3

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
-#include "relie.h"
+#include "SSP.h"
 
 int main() {
-    char nom_fichier[50];
-    graphe mongraphe;
-    int point_depart = 0;
-    int point_arrive = 0;
-    printf("Veiller saisir le nom du fichier\n");
-    scanf("%[^\n]%*c", nom_fichier);
-    //ouverture_du_fichier(&mongraphe, nom_fichier);
-    printf("\ntaille du graphe: %d", trouvTaille(nom_fichier));
-    printf("\nordre du graphe: %d", trouvOrdre(nom_fichier));
-    return 0;
+    char *fileExclu;
+     G* grapheExclu;
+
+     fileExclu="exclusion.txt";
+     grapheExclu=main_ex(fileExclu);
+     menu(fileExclu);
+     return 0;
 }
